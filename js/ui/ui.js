7a49c5c721bf62e92ec3db107fb04021d17e76ab
Ian Page Hands <ihands@redhat.com>
2014-06-06 09:51:18 -0400

    move to haml and fix percent input

--- js/ui/ui.js
+++ js/ui/ui.js
@@ -2,12 +2,12 @@
 
 (function () {
     angular.module("ajnachakra", []).controller("UiController", function ($scope) {
-        $scope.fudge = 0.08;
+        $scope.fudge = 8;
         $scope.skip = 3;
         require(['detector'], function (detector) {
             $scope.$watch(function () {
-                if ($scope.fudge >= 0.01 && $scope.fudge <= 0.8) {
-                    detector.settings.fudge = $scope.fudge;
+                if ($scope.fudge >= 1 && $scope.fudge <= 80) {
+                    detector.settings.fudge = $scope.fudge / 100.0;
                 }
 
                 if ($scope.skip > 0) {
